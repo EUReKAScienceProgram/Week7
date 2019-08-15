93815ed8d79a5bcae4cbce45aa140eb953b875a1
EUReKACamper13 <51757403+EUReKACamper@users.noreply.github.com>
2019-08-15 15:47:49 -0700

    more stuff

---
+++ Projects/BlackWidow/ProgrammingIntro.java
@@ -0,0 +1,22 @@
+public class ProgrammingIntro {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int result = myFunction (64, 54);
+        System.out.println(result);
+        // int thisIsAVariable = 17;
+        // double decimalNumber = 42.68;
+        // boolean trueOrFalse = true;
+        // char aLetter = 'N';
+        // String someText = "Basketball";
+
+        // System.out.println(thisIsAVariable);
+        // System.out.println(decimalNumber);
+        // System.out.println(trueOrFalse);
+        // System.out.println(aLetter);
+        // System.out.println(someText);
+}
+public static int myFunction(int numberOne, int numberTwo){
+    return numberOne * numberTwo;
+
+}
+}
