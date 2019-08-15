335a9427a8d9fe3477f8408fb637d8a7af4577fc
EUReKACamper15 <51757403+EUReKACamper@users.noreply.github.com>
2019-08-15 15:36:28 -0700

    stuff

---
+++ Projects/Star-Lord/ProgrammingIntro.java
@@ -0,0 +1,24 @@
+public class ProgrammingIntro {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int result = myFunction (5,10); 
+        System.out.println(result);
+        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+        // int thisIsAVariable = 6; 
+        // double decimalNumber = 3.7; 
+        // boolean trueOrFalse = true; 
+        // char aletter = 'N'; 
+        // String someText = "Wassup";
+        
+        // System.out.println(thisIsAVariable);
+        // System.out.println(decimalNumber);
+        // System.out.println(trueOrFalse);
+        // System.out.println(someText);                                         
+
+}
+public static int myFunction(int numberOne, int numberTwo){
+    return numberOne * numberTwo; 
+
+}
+}
