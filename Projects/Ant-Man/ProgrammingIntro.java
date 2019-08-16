3e8a452e4d0ca0284fc024fa0aba1c2359ec024c
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-08-16 13:43:50 -0700

    last push

---
+++ Projects/Ant-Man/ProgrammingIntro.java
@@ -0,0 +1,22 @@
+public class ProgrammingIntro{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int result = myFunction (18649,548227);
+        System.out.println(result);
+        // int thisIsAVariable = 10100010;
+        // double decimalNumber = 24.87;
+        // boolean trueOrFalse = true;
+        // char aLetter = 'U';
+        // String someText = "Incredible";
+
+        // System.out.println(thisIsAVariable);
+        // System.out.println(decimalNumber);
+        // System.out.println (trueOrFalse);
+        // System.out.println(aLetter);
+        // System.out.println(someText);
+}
+public static int myFunction(int numberOne, int numberTwo){
+    return numberOne * numberTwo;
+
+}
+}
