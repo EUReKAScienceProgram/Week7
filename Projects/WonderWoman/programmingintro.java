93be1598611fe1c2fdbe2a9753c3ad749fc52f6a
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-08-19 07:15:11 -0700

    Create programmingintro.java

---
+++ Projects/WonderWoman/programmingintro.java
@@ -0,0 +1,22 @@
+public class programmingintro {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int result = myFunction (5,10);
+        System.out.println(result);
+//         int thisIsAVariable = 10;
+//         double decimalNumber = 13.95;
+//         boolean trueOrFalse = true;
+//         char aletter = 'M';
+//         String someText = "Minecraft!";
+
+//         System.out.println(thisIsAVariable);
+//         System.out.println(decimalNumber);
+//         System.out.println(trueOrFalse);
+//         System.out.println(aletter);
+//         System.out.println(someText);
+ }
+public static int myFunction(int numberOne, int numberTwo) {
+return numberOne * numberTwo;
+
+}
+}
