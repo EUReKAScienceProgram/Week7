25de4f87122814b200db7cb88eedbec13c016498
EUReKACamper18 <51757403+EUReKACamper@users.noreply.github.com>
2019-08-19 08:10:41 -0700

    Create ProgrammingIntro.java

---
+++ Projects/CaptainMarvel/ProgrammingIntro.java
@@ -0,0 +1,22 @@
+public class ProgrammingIntro {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int result = myFunction (47, 23);
+        System.out.println(result);
+        // int thisIsAVariable = 7;
+        // double decimalNumber = 68.43;
+        // boolean trueORFalse = true;
+        // char aLetter = 'v';
+        // String someText = "superdexahedron!";
+
+        // System.out.println(thisIsAVariable);
+        // System.out.println(decimalNumber);
+        // System.out.println(trueORFalse);
+        // System.out.println(aLetter);
+        // System.out.println(someText);
+}
+public static int myFunction(int numberOne, int numberTwo){
+    return numberOne * numberTwo;
+
+}
+}
